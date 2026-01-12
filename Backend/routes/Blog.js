8d9a66969b795996ce663b6a2a43538fbb921911
akash-sharma8 <sharmaakash8218@gmail.com>
2026-01-12 15:44:04 +0530

    Add blog routes

--- Backend/routes/Blog.js
+++ Backend/routes/Blog.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const {createBlog, getBlogs,getMyBlog, getSingleBlog, updateBlog, deleteBlog,likesBlog,commentBlog} = require('../controllers/blogController');
+const {
+  createBlog,
+  getBlogs,
+  getMyBlog,
+  getSingleBlog,
+  updateBlog,
+  deleteBlog,
+  likesBlog,
+  commentBlog
+} = require('../controllers/blogController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/blogs', authMiddleware, createBlog);
-
 router.get('/blogs', getBlogs);
-
 router.get('/blogs/:id', getSingleBlog);
-
 router.get('/myblogs', authMiddleware, getMyBlog);
-
-router.put("/blogs/:id/like", authMiddleware, likesBlog);
-
+router.put('/blogs/:id/like', authMiddleware, likesBlog);
 router.post('/blogs/:id/comment', authMiddleware, commentBlog);
-
 router.put('/blogs/:id', authMiddleware, updateBlog);
-
 router.delete('/blogs/:id', authMiddleware, deleteBlog);
-router.get('/blogs', (req, res) => {
-    res.json({ msg: "Blogs route works" });
-  });
 
 module.exports = router;
