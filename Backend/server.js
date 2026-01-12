5d585f3a5bcae043af0ebff4584b9a76c0da04bd
akash-sharma8 <sharmaakash8218@gmail.com>
2026-01-12 19:32:42 +0530

    main

--- Backend/server.js
+++ Backend/server.js
@@ -35,15 +35,9 @@ app.use(
 );
 
 
+
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins.join(","));
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 
 // Routes
