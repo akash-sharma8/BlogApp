0a0309d70bf95952cbec6cf79dc2ba881b61039c
akash-sharma8 <sharmaakash8218@gmail.com>
2026-01-19 18:55:43 +0530

    add

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -6,14 +6,12 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // Load user & token from localStorage on app start
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
     if (storedUser) setUser(storedUser);
     setLoading(false);
   }, []);
 
-  // Login function
   const login = async (email, password) => {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
       method: "POST",
@@ -21,32 +19,21 @@ export const AuthProvider = ({ children }) => {
       body: JSON.stringify({ email, password }),
     });
 
-
     const data = await res.json();
     if (!res.ok) throw new Error(data.message || "Login failed");
 
-    const userWithToken = {
-      ...data.user,
-      token: data.token,
-    };
-
+    const userWithToken = { ...data.user, token: data.token };
     setUser(userWithToken);
     localStorage.setItem("user", JSON.stringify(userWithToken));
-
-
 
     return data;
   };
 
-
-  // Logout function
   const logout = () => {
     setUser(null);
     localStorage.removeItem("user");
   };
 
-
-  // Register function
   const register = async (username, email, password) => {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
       method: "POST",
@@ -54,26 +41,18 @@ export const AuthProvider = ({ children }) => {
       body: JSON.stringify({ username, email, password }),
     });
 
-
     const data = await res.json();
     if (!res.ok) throw new Error(data.message || "Registration failed");
 
-    const userWithToken = {
-      ...data.user,
-      token: data.token
-    };
-
+    const userWithToken = { ...data.user, token: data.token };
     setUser(userWithToken);
     localStorage.setItem("user", JSON.stringify(userWithToken));
 
     return data;
   };
 
-
   return (
-    <AuthContext.Provider
-      value={{ user, loading, setUser, login, logout, register }}
-    >
+    <AuthContext.Provider value={{ user, loading, login, logout, register }}>
       {children}
     </AuthContext.Provider>
   );
